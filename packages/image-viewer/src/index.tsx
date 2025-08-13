d369e756af126cc3cffc6428227083c239fa5d50
There Is No Knowledge That Is Not Power <tinktinp@proton.me>
2025-08-13 00:32:10 -0500

    fix service worker scope

--- packages/image-viewer/src/index.tsx
+++ packages/image-viewer/src/index.tsx
@@ -14,26 +14,24 @@ root.render(
     </StrictMode>,
 );
 
-
 const registerServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register(new URL('./service-worker.ts', import.meta.url), {
-        scope: "/",
-      });
-      await registration.update();
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register(
+                new URL('./service-worker.ts', import.meta.url),
+            );
+            await registration.update();
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
     }
-  }
 };
-
 
 registerServiceWorker();
